6c6dfe5a2fbe8c651f8a9c194fb6a38ec69eae3b
Almas Baim <almaslvl@gmail.com>
2024-12-24 08:49:46 +0000

    initial commit

---
+++ src/main/java/com/almasb/fxglgames/drop/ExampleApp.java
@@ -0,0 +1,45 @@
+package com.almasb.fxglgames.drop;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.texture.GLImageView;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+/**
+ * @author Almas Baim (https://github.com/AlmasB)
+ */
+public class ExampleApp extends GameApplication {
+
+    private static final String SIMPLE_SHADER = """
+            #version 460
+            
+            uniform float red;
+            
+            void main() {
+                gl_FragColor = vec4(red, 0.0, 0.0, 1.0);
+            }
+            
+            """;
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(1280);
+        settings.setHeight(720);
+    }
+
+    @Override
+    protected void initGame() {
+        GLImageView view = new GLImageView(1280, 720, SIMPLE_SHADER);
+        view.getProperties().setValue("red", 0.5);
+
+        entityBuilder()
+                .at(0, 0)
+                .view(view)
+                .buildAndAttach();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
